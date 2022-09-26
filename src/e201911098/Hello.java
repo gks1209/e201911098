332b03e6609dc5013e41eccea5552c20cda0e871
Han, Jongsu <gks12090607@gmail.com>
2022-09-26 17:23:06 +0900

    Main.java 수정#1

--- src/e201911098/Hello.java
+++ src/e201911098/Hello.java
@@ -1,8 +1,6 @@
 package e201911098;
 
 public class Hello {
-	String msg = "hello";
-
 	public void say() {
 		System.out.println("hello");
 	}
--- src/e201911098/Main.java
+++ src/e201911098/Main.java
@@ -3,7 +3,8 @@ package e201911098;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Hello hello = new Hello(); 
+		hello.say();
 	}
 
 }
